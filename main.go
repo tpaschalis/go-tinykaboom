94dac0d54bb5d391b0f40bff427b8cdb3ff1a215
Paschalis T <paschalist0@gmail.com>
2019-01-28 20:53:52 +0200

    Step 7, the final one: fire colors. I'm not sure if the ramp of colors is working correctly though....

--- main.go
+++ main.go
@@ -73,6 +73,14 @@ func lerp (v0, v1, t float64) float64 {
 }
 
 
+func lerpColor (v0, v1 color.RGBA, t float64) color.RGBA {
+    return color.RGBA{ v0.R + (v1.R-v0.R) * uint8(max(0., min(1.,t))),
+                       v0.G + (v1.G-v0.G) * uint8(max(0., min(1.,t))),
+                       v0.B + (v1.B-v0.B) * uint8(max(0., min(1.,t))),
+                       255 }
+}
+
+
 func noise(x r3.Vector) float64 {
     p := r3.Vector{math.Floor(x.X), math.Floor(x.Y), math.Floor(x.Z)}
     f := r3.Vector{x.X-p.X, x.Y-p.Y, x.Z-p.Z}
@@ -116,6 +124,25 @@ func fractalBrownianMotion(x r3.Vector) float64 {
     return f/0.9375;
 }
 
+func paletteFire(d float64) color.RGBA {
+
+    yellow := color.RGBA{uint8(255), uint8(250), uint8(0), uint8(255)} // "note that the color is "hot", i.e. has components >255"
+    orange := color.RGBA{uint8(255), uint8(150), uint8(0), uint8(255)}
+    red := color.RGBA{uint8(255), uint8(0), uint8(0), uint8(255)}
+    darkGray := color.RGBA{uint8(50), uint8(50), uint8(50), uint8(255)}
+    gray := color.RGBA{uint8(100), uint8(100), uint8(100), uint8(255)}
+
+    x := max(0., min(1., d))
+    if x<0.25 {
+        return lerpColor(gray, darkGray, x*4.0)
+    } else if x<0.5 {
+        return lerpColor(darkGray, red, x*4.0 - 1.0)
+    } else if x<0.75 {
+        return lerpColor(red, orange, x*4.0 - 2.0)
+    }
+    return lerpColor(orange, yellow, x*4.0 - 3.0)
+}
+
 func main() {
     const width = 640.
     const height = 480.
@@ -123,6 +150,7 @@ func main() {
 
     backgroundColor := color.RGBA{51, 178, 204, 255}
     whiteColor := color.RGBA{255, 255,255, 255}
+    _ = whiteColor
 
 	f, _ := os.OpenFile("out.png", os.O_WRONLY|os.O_CREATE, 0600)
 	defer f.Close()
@@ -137,12 +165,14 @@ func main() {
             dirZ = -height/(2.*math.Tan(fov/2.))
 
             var hit r3.Vector
-            if sphereTrace(r3.Vector{0, 0, 3}, r3.Vector.Normalize(r3.Vector{dirX, dirY, dirZ}), &hit) {
-                //img.Set(int(i), int(j), color.RGBA{255, 255, 255, 255})
+            if sphereTrace(r3.Vector{0, 0, 3}, r3.Vector.Normalize(r3.Vector{dirX, dirY, dirZ}), &hit) {    // Camera is placed on {0, 0, 3}
+                noiseLevel := (sphereRadius - r3.Vector.Norm(hit))/noiseAmplitude
                 lightSrc := r3.Vector{10, 10, 10}
                 lightDir := r3.Vector.Normalize(r3.Vector.Sub(lightSrc, hit))
                 lightIntensity := max(0.4, r3.Vector.Dot(lightDir,distanceFieldNormal(hit)))
-                img.Set(int(i), int(j), multiplyColorIntensity(whiteColor, lightIntensity))
+                //img.Set(int(i), int(j), multiplyColorIntensity(whiteColor, lightIntensity))
+                currColor := paletteFire((-0.2 + noiseLevel)*2)
+                img.Set(int(i), int(j), multiplyColorIntensity(currColor, lightIntensity))
             } else {
                 img.Set(int(i), int(j), backgroundColor)
             }
